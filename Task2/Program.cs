8fdd66ed816a9dc2ba97a50bade56faaeb40af81
Iryna <batsianiryna@gmail.com>
2022-12-09 22:37:35 +0300

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+
+Console.Clear();
+Console.Write("Введите длину первой стороны треугольника: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длину второй стороны треугольника: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длину третьей стороны треугольника: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < (b + c) && b < (a + c) && c < (a + b))
+  Console.WriteLine("YES");
+else
+  Console.WriteLine("NO");
