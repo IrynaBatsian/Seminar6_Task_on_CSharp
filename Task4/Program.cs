354473e807f51af4d8b5915d2e5fd52830e0669e
Iryna <batsianiryna@gmail.com>
2022-12-09 23:03:04 +0300

    add task4 вывод чисел Фибоначчи

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x, a0 = 0, a1 = 1;
+for (int i = 0; i < n; i++)
+{
+  Console.Write($"{a0} ");
+  x = a0 + a1;
+  a0 = a1;
+  a1 = x;
+}
